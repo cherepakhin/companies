dd3ff61e1e4d38e985cf92439cef3a57b562fdfa
Черепахин Василий <vasi.che@gmail.com>
2023-10-10 18:56:15 +0500

    add Readme.md

---
+++ src/test/java/ru/perm/v/companies/CompaniesApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.perm.v.companies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CompaniesApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1, 1);
+	}
+
+}
