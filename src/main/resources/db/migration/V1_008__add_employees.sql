952a745c6718a8b8a7f14ca2240a55de992449a8
Черепахин Василий <vasi.che@gmail.com>
2023-10-15 19:12:55 +0500

    add V1_008__add_employees.sql

---
+++ src/main/resources/db/migration/V1_008__add_employees.sql
@@ -0,0 +1,5 @@
+insert into employee (firstname, lastname, fathername, birthday) values
+ ('firstname_30', 'lastname_30', 'fathername_30', '2020-03-03'),
+ ('firstname_20', 'lastname_20', 'fathername_20', '2020-02-02')
+  ;
+ commit;
