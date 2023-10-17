7864d9b684ce631927c43e1d432aa6de14213fc3
Черепахин Василий <vasi.che@gmail.com>
2023-10-17 16:50:11 +0500

    add CompanyRest.java

---
+++ src/main/java/ru/perm/v/companies/rest/CompanyRest.java
@@ -0,0 +1,50 @@
+package ru.perm.v.companies.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.perm.v.companies.dto.CompanyDto;
+import ru.perm.v.companies.service.CompanyService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@Slf4j
+@RequestMapping("/company")
+public class CompanyRest {
+
+    private CompanyService companyService;
+
+    public CompanyRest(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping("/")
+    public List<CompanyDto> getAll() {
+        log.info("get /company/getAll");
+        return companyService
+                .getAll()
+                .stream()
+                .map(c -> new CompanyDto(
+                        c.getN(),
+                        c.getShortname(),
+                        c.getFullname(),
+                        c.getInn(),
+                        c.getOgrn(),
+                        c.getAddressPost(),
+                        c.getAddressUr()
+                ) {
+                })
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public CompanyDto getById(@PathVariable Long id) {
+        log.info(String.format("get /company/getById/%d", id));
+        return companyService.getByN(id);
+    }
+
+}
---
+++ src/test/java/ru/perm/v/companies/rest/CompanyRestTest.java
@@ -0,0 +1,41 @@
+package ru.perm.v.companies.rest;
+
+import org.junit.jupiter.api.Test;
+import ru.perm.v.companies.dto.CompanyDto;
+import ru.perm.v.companies.service.CompanyService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CompanyRestTest {
+
+    CompanyService companyService = mock(CompanyService.class);
+
+    @Test
+    public void getAll() {
+        CompanyRest rest = new CompanyRest(companyService);
+        CompanyDto company1 = new CompanyDto();
+        company1.setN(1L);
+        CompanyDto company2 = new CompanyDto();
+        company2.setN(2L);
+
+        when(companyService.getAll()).thenReturn(List.of(company1, company2));
+
+        assertEquals(List.of(company1, company2), rest.getAll());
+    }
+
+    @Test
+    public void getById() {
+        Long ID = 100L;
+        CompanyRest rest = new CompanyRest(companyService);
+        CompanyDto company1 = new CompanyDto();
+        company1.setN(ID);
+
+        when(companyService.getByN(ID)).thenReturn(company1);
+
+        assertEquals(company1, rest.getById(ID));
+    }
+}
