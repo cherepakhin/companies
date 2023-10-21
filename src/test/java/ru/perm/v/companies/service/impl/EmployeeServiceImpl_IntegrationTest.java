d3312ca0c43b52b25a9f2347bab1ccabe567e0c4
Черепахин Василий <vasi.che@gmail.com>
2023-10-21 13:55:14 +0500

    add EmployeeServiceImpl_IntegrationTest.findAllByOrderByNAsc()

--- src/test/java/ru/perm/v/companies/service/impl/EmployeeServiceImpl_IntegrationTest.java
+++ src/test/java/ru/perm/v/companies/service/impl/EmployeeServiceImpl_IntegrationTest.java
@@ -10,8 +10,7 @@ import ru.perm.v.companies.service.EmployeeService;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 //TODO: add config with fake data for test
 @SpringBootTest
@@ -41,8 +40,14 @@ public class EmployeeServiceImpl_IntegrationTest {
     @Test
     public void findAllByOrderByNAsc() {
         List<EmployeeDto> empls = employeeService.findAllByOrderByNAsc();
+
         assertEquals(6, empls.size());
-        assertArrayEquals(List.of(0L, 1L, 2L, 3L, 4L, 5L).toArray(), empls.stream().map(EmployeeDto::getN).collect(Collectors.toList()).toArray());
+
+        assertArrayEquals(List.of(0L, 1L, 2L, 3L, 4L, 5L).toArray(),
+                empls.stream().map(EmployeeDto::getN).toArray());
+
+        List<Long> listN = empls.stream().map(EmployeeDto::getN).collect(Collectors.toList());
+        assertEquals(List.of(0L, 1L, 2L, 3L, 4L, 5L), listN);
     }
 
     @Test
