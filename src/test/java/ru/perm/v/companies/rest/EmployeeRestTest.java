c79ef821f1daaa42b8264ebc9bfab8754697b2c4
Черепахин Василий <vasi.che@gmail.com>
2023-10-22 18:56:56 +0500

    modify test  EmployeeRestTest.getAll

--- src/test/java/ru/perm/v/companies/rest/EmployeeRestTest.java
+++ src/test/java/ru/perm/v/companies/rest/EmployeeRestTest.java
@@ -14,16 +14,16 @@ public class EmployeeRestTest {
 
     @Test
     public void getAll() {
-        Long N1 = 1L;
-        String FIRST_NAME_1 = "FIRST_NAME_1";
-        Long N2 = 2L;
-        String FIRST_NAME_2 = "FIRST_NAME_2";
         EmployeeDto employee1 = new EmployeeDto();
-        employee1.setN(N1);
-        employee1.setFirstname(FIRST_NAME_1);
+        employee1.setN(1L);
+        employee1.setFirstname("FIRST_NAME_1");
+        employee1.setLastname("LAST_NAME_1");
+        employee1.setFathername("FATHER_NAME_1");
         EmployeeDto employee2 = new EmployeeDto();
-        employee2.setN(N2);
-        employee2.setFirstname(FIRST_NAME_2);
+        employee2.setN(2L);
+        employee2.setLastname("LAST_NAME_2");
+        employee2.setFirstname("FIRST_NAME_2");
+        employee2.setFathername("FATHER_NAME_2");
 
         EmployeeService employeeService = mock(EmployeeService.class);
         when(employeeService.getAll()).thenReturn(List.of(employee1, employee2));
@@ -32,9 +32,11 @@ public class EmployeeRestTest {
         List<EmployeeDto> receivedEmpls = employeeRest.getAll().getBody();
 
         assertEquals(2, receivedEmpls.size());
-        EmployeeDto receivedEmpls0 = receivedEmpls.get(0);
         assertEquals(
-                new EmployeeDto(1L, "FIRST_NAME_1", "", "", "1990/01/01"),
-                receivedEmpls0);
+                new EmployeeDto(1L, "FIRST_NAME_1", "LAST_NAME_1", "FATHER_NAME_1", "1990/01/01"),
+                receivedEmpls.get(0));
+        assertEquals(
+                new EmployeeDto(2L, "FIRST_NAME_2", "LAST_NAME_2", "FATHER_NAME_2", "1990/01/01"),
+                receivedEmpls.get(1));
     }
 }
