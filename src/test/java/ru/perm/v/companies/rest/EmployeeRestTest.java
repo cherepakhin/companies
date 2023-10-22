ddd8a57b761b14b483b35f05861ccd8f77a4292b
Черепахин Василий <vasi.che@gmail.com>
2023-10-22 19:00:19 +0500

    modify test  EmployeeRestTest.getAll

--- src/test/java/ru/perm/v/companies/rest/EmployeeRestTest.java
+++ src/test/java/ru/perm/v/companies/rest/EmployeeRestTest.java
@@ -19,11 +19,13 @@ public class EmployeeRestTest {
         employee1.setFirstname("FIRST_NAME_1");
         employee1.setLastname("LAST_NAME_1");
         employee1.setFathername("FATHER_NAME_1");
+        employee1.setBirthday("2021/01/01");
         EmployeeDto employee2 = new EmployeeDto();
         employee2.setN(2L);
         employee2.setLastname("LAST_NAME_2");
         employee2.setFirstname("FIRST_NAME_2");
         employee2.setFathername("FATHER_NAME_2");
+        employee2.setBirthday("2022/12/02");
 
         EmployeeService employeeService = mock(EmployeeService.class);
         when(employeeService.getAll()).thenReturn(List.of(employee1, employee2));
@@ -33,10 +35,10 @@ public class EmployeeRestTest {
 
         assertEquals(2, receivedEmpls.size());
         assertEquals(
-                new EmployeeDto(1L, "FIRST_NAME_1", "LAST_NAME_1", "FATHER_NAME_1", "1990/01/01"),
+                new EmployeeDto(1L, "FIRST_NAME_1", "LAST_NAME_1", "FATHER_NAME_1", "2021/01/01"),
                 receivedEmpls.get(0));
         assertEquals(
-                new EmployeeDto(2L, "FIRST_NAME_2", "LAST_NAME_2", "FATHER_NAME_2", "1990/01/01"),
+                new EmployeeDto(2L, "FIRST_NAME_2", "LAST_NAME_2", "FATHER_NAME_2", "2022/12/02"),
                 receivedEmpls.get(1));
     }
 }
