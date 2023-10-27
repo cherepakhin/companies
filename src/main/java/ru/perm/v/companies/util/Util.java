7c533b1dd9b1033110ea68ac013a36f947bc73a7
Черепахин Василий <vasi.che@gmail.com>
2023-10-27 16:31:39 +0500

    add comment about format dd/MM/yyyy

--- src/main/java/ru/perm/v/companies/util/Util.java
+++ src/main/java/ru/perm/v/companies/util/Util.java
@@ -6,10 +6,16 @@ import java.time.format.DateTimeFormatter;
 public class Util {
     public static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
+    /**
+     * Convert from String to LocalDate. String format dd/MM/yyyy
+     */
     public static LocalDate fromStringToDate(String str) {
         return LocalDate.parse(str, dateTimeFormatter);
     }
 
+    /**
+     * Convert from LocalDate to String. String format dd/MM/yyyy
+     */
     public static String fromDateToString(LocalDate ddate) {
         return dateTimeFormatter.format(ddate);
     }
