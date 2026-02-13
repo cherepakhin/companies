0d049784ec56ae9e7180d783c78bdd35eb80debd
vasi <vasi.che@gmail.com>
2026-02-13 17:56:46 +0500

    test

--- src/test/java/ru/perm/v/companies/rest/CompanyRestTest.java
+++ src/test/java/ru/perm/v/companies/rest/CompanyRestTest.java
@@ -1,6 +1,5 @@
 package ru.perm.v.companies.rest;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
